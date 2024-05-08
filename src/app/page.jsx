eb4ddd1a4d40ee35a2ef3120534d8c84866ea838
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-05-08 11:59:24 -0400

    Creating a QR Code

---
+++ src/app/page.jsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-blue-100">
+      <div className="card card-compact w-96 bg-base-100 shadow-xl">
+        <figure>
+          <Image
+            src={"/images/image-qr-code.png"}
+            width={500}
+            height={500}
+            alt="QR CODE"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title justify-center">QR Code</h2>
+          <p className="flex justify-center">Please scan the QR Code above</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
